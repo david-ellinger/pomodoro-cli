c32151b5928fb52eb90db4b76a145d7bc4727650
david-ellinger <43858539+david-ellinger@users.noreply.github.com>
2021-12-17 07:41:52 -0500

    Setup (#1)
    * Existing
    
    * Added redis UI
    
    * Docs

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
 # Pomodoro App
 
-## TODO
+## Reference
 
-- Add Users
-- Play Sound effects
-- Add ability to add tags
-- Show statistics
-- Reset Data
+https://www.jhonatandasilva.com/published/1627031260
+https://redis.io/documentation
+https://hackernoon.com/how-to-configurate-redis-redis-commander-docker-616136f2
+
+https://pypi.org/project/redis/
+https://www.jhonatandasilva.com/permanent/1626532688
--- src/main.py
+++ src/main.py
@@ -1,63 +1,73 @@
 import typer
 import time
-from db import Database
+import redis
 
 app = typer.Typer()
-db = Database()
+db = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 db.set("completed_times", 0)
 
 
 @app.command()
-def init(default_time:int = typer.Option(25, prompt=True), short_break: int = typer.Option(5, prompt=True), long_break: int = typer.Option(15, prompt=True)) -> None:
+def init(
+    default_time: int = typer.Option(25, prompt=True),
+    short_break: int = typer.Option(5, prompt=True),
+    long_break: int = typer.Option(15, prompt=True),
+) -> None:
     data = {
         "default_time": default_time,
         "short_break": short_break,
-        "long_break": long_break
+        "long_break": long_break,
     }
     create = typer.confirm(f"Are you sure? {data=}")
     if not create:
         typer.echo("Not creating")
         raise typer.Abort()
     typer.echo("Creating...")
-    db.set_redis_values(data)
+    set_redis_values(data)
+
 
 @app.command()
 def start():
-  data = get_data()
-  break_time = data["completed_times"]
-  default_time = data["default_time"]
-  typer.echo("[*] Started Pomodoro")
-  incremental_sleep(default_time)
+    data = get_data()
+    break_time = data["completed_times"]
+    default_time = data["default_time"]
+    typer.echo("[*] Started Pomodoro")
+    incremental_sleep(default_time)
 
-  typer.echo("[*] Completed Pomodoro")
-  db.db.incrby("completed_times", 1)
-  start_rest = typer.confirm("Start rest?")
-  if not start_rest:
-      typer.echo("Existing...")
-      typer.Abort()
-  incremental_sleep(break_time)
-  typer.echo("[*] Session Finished")
+    typer.echo("[*] Completed Pomodoro")
+    db.incrby("completed_times", 1)
+    start_rest = typer.confirm("Start rest?")
+    if not start_rest:
+        typer.echo("Existing...")
+        typer.Abort()
+    incremental_sleep(break_time)
+    typer.echo("[*] Session Finished")
 
-def incremental_sleep(sleep_time:int) -> None:
-  pomodoro_chunks = [1 for _ in range(sleep_time)]
-  for t in pomodoro_chunks:
-      time.sleep(60*t)
-      typer.echo("[-] 1 min passed")
+
+def incremental_sleep(sleep_time: int) -> None:
+    pomodoro_chunks = [1 for _ in range(sleep_time)]
+    for t in pomodoro_chunks:
+        time.sleep(60 * t)
+        typer.echo("[-] 1 min passed")
+
 
 def get_data() -> dict:
     data = {
-        "fourth_time":False,
-        "break_time":0,
+        "fourth_time": False,
+        "break_time": 0,
     }
-    data["break_time"] = db.db.incrby("short_break", 0)
-    data["default_time"] = db.db.incrby("default_time", 0)
+    data["break_time"] = db.incrby("short_break", 0)
+    data["default_time"] = db.incrby("default_time", 0)
 
-    completed_times = db.db.incry("complted_times", 0)
+    completed_times = db.incry("complted_times", 0)
     if completed_times and completed_times % 4 == 0:
-        data["break_time"] = db.db.incrby("long_break",0)
+        data["break_time"] = db.incrby("long_break", 0)
         data["fourth_time"] = True
 
     return data
+
+def set_redis_values(data):
+    [db.set(key, data[key]) for key in data.keys()]
+
 if __name__ == "__main__":
     app()
-
