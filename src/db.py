bdc7fc955c85e5d51127c61fca7028c1bec0c4dd
David Ellinger <david.ellinger@me.com>
2021-12-16 22:50:09 -0500

    Existing

--- src/db.py
+++ src/db.py
@@ -1,11 +1,11 @@
 import redis
 
-class Database():
-    db = redis.Redis(host="redis",port=6379, db=0, decode_responses=True)
+
+class Database:
+    db = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
     def set(self, key, value, **kwargs):
         return self.db.set(key, value, **kwargs)
 
     def set_redis_values(self, data):
         [self.db.set(key, data[key]) for key in data.keys()]
-
